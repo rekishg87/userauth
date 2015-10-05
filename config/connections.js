50fd889228f4df0609f73f4835833f6bca98979d
Rekish <rekishg87@gmail.com>
2015-10-05 15:19:42 +0200

    Completed Video09  / Video09 054 Switching to MongoDB

--- config/connections.js
+++ config/connections.js
@@ -56,13 +56,13 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  someMongodbServer: {
+  myMongo: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    host: 'ds029454.mongolab.com',
+    port: 29454,
+    user: 'rekish',
+    password: '123456',
+    database: 'userauth'
   },
 
   /***************************************************************************
