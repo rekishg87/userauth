7bf1519ed5acf147f59487cc7c442910e109171c
Rekish <rekishg87@gmail.com>
2015-10-05 11:04:43 +0200

    Completed Video01 / Video09

---
+++ assets/js/public/signup/SignupCtrl.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Rekish on 10/5/2015.
+ */
--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -28,6 +28,8 @@ var jsFilesToInject = [
 
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
+  'js/public/signup/SignupMod.js',
+  'js/public/signup/SignupCtrl.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
